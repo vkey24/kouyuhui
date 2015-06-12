a9cbd2bc81234f4818ec5574faa6b4b1cbb0e23b
vkey24 <vkey24@hotmail.com>
2015-06-12 18:15:21 +0800

    口语汇_swift-app

---
+++ 口语汇/口语汇/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  口语汇
+//
+//  Created by jason on 15/6/12.
+//  Copyright (c) 2015年 JunHong.Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var account: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    //用户登录判断
+    @IBAction func doCheckLogin(sender: AnyObject) {
+        
+    }
+    
+
+}
+
